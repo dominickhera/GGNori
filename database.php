52080fd07750e6a6e4db2f1745ae5e419f105ccb
Dominick Hera <dominickhera@me.com>
2018-08-25 00:14:10 -0400

    lets see if database works

---
+++ database.php
@@ -0,0 +1,24 @@
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+    'dsn' => '',
+    'hostname' => 'us-cdbr-iron-east-01.cleardb.net',
+    'username' => 'b52246f2fbaf21',
+    'password' => 'c0717918',
+    'database' => 'heroku_f18a66d54326764',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVRIONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'striction' => FALSE,
+    'fallover' => array(),
+    'save_querries' => TRUE
+);
