d890680821ba0cf242639c02e5ade6f23e38ab52
Dominick Hera <dominickhera@me.com>
2018-10-01 18:11:41 -0400

    attemtpt to work

--- public/assets/js/championData.js
+++ public/assets/js/championData.js
@@ -72,6 +72,19 @@ testConsolePrint = function(data) {
   console.log("also heres just data: "+ data);
 }
 
+getBase64Image = function(img) {
+  var canvas = document.createElement("canvas");
+  canvas.width = img.width;
+  canvas.height = img.height;
+
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0);
+
+  var dataURL = canvas.toDataURL("image/png");
+
+  return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
+
 /**
  *  Parse requests handler
  */
